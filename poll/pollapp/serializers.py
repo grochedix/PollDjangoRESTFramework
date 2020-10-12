0fdfcdea735e46fb3b45538c96c7438e2558bf3a
Guillaume <guillaume.rochedix@gmail.com>
2020-10-12 21:01:12 +0200

    black

--- poll/pollapp/serializers.py
+++ poll/pollapp/serializers.py
@@ -3,14 +3,16 @@ from .models import Question, Answer
 from rest_framework import serializers
 from rest_framework_recursive.fields import RecursiveField
 
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ["url", "username", "email"]
-        
+
 
 class AnswerSerializer(serializers.ModelSerializer):
     subanswers = RecursiveField(required=False, allow_null=True, many=True)
+
     class Meta:
         model = Answer
         fields = ["id", "text", "subanswers"]
